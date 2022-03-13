2d8f747c89a76438c538952cca986420d397c0df
matiasangelani <matiasangelani2@gmail.com>
2022-03-12 21:42:10 -0300

    Recipe.js modified

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -1,12 +1,31 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('recipe', {
+  sequelize.define("Recipe", {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    dishSummary: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    points: {
+      type: DataTypes.INTEGER,
+    },
+    healthy: {
+      type: DataTypes.INTEGER,
+    },
+    instructions: {
+      type: DataTypes.TEXT,
+    },
+    created: {
+      type: DataTypes.VIRTUAL,
+      get() {
+        return true;
+      },
     },
   });
 };
